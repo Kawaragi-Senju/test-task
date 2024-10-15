474e902f0947d87767a2a57c3d10a961d3172917
danny <dannyboykov@bk.ru>
2024-10-15 19:37:13 +0300

    Test task

--- src/main/java/com/example/testtask/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.testtask.repository;
-
-import com.example.testtask.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
